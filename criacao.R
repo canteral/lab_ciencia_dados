b0b0f686aa58be044a63c4e1d56e30c6fe693c08
canteral <canteralkleve@gmail.com>
2021-04-14 21:08:24 -0300

    Initial commit

--- criacao.R
+++ criacao.R
@@ -2,3 +2,6 @@
 
 # Criar  estrutura GIT para este projeto
 usethis::use_git()
+usethis::use_github()
+usethis::create_github_token()
+gitcreds::gitcreds_set(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)
